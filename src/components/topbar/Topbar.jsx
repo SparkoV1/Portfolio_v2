c4d4cebbc884142b8c7695c560e4b2512e8e3cd3
SparkoV1 <79722860+SparkoV1@users.noreply.github.com>
2021-07-28 22:38:47 +0200

    Portfolio_v2

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import {Person, Mail} from '@material-ui/icons';
+
+const Topbar = ({menuOpen, setMenuOpen}) => {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">DZ.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+48 793 085 586</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>zelenskyidmytro1@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Topbar;
